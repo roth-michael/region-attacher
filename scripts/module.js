5f846bf610212d5cf517f677e70293d75bb93182
Michael Roth <michaelhroth624@gmail.com>
2024-06-09 22:27:24 -0400

    bugfix

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,9 @@
 import registerHooks from "./hooks.js";
+import registerSheetOverrides from "./sheet-overrides.js";
 
 Hooks.once('init', async function() {
     registerHooks();
+    registerSheetOverrides();
 });
 
 Hooks.once('ready', async function() {
--- scripts/sheet-overrides.js
+++ scripts/sheet-overrides.js
@@ -1,16 +1,17 @@
 import CONSTANTS from "./constants.js";
 
 export default function registerSheetOverrides() {
-    Hooks.on("renderItemSheet5e", patchItemSheet());
+    Hooks.on("renderItemSheet5e", patchItemSheet);
 }
 
-function getAttachRegionHtml() {
+function getAttachRegionHtml(item) {
+    let attachRegionToTemplate = foundry.utils.getProperty(item, CONSTANTS.FLAGS.ATTACH_REGION_TO_TEMPLATE) ?? false;
     return `
         <div class="form-group">
             <label>${game.i18n.localize("REGION-ATTACHER.RegionAttacher")}</label>
             <div class="form-fields">
                 <label class="checkbox">
-                    <input type="checkbox" name="${CONSTANTS.FLAGS.ATTACH_REGION_TO_TEMPLATE}">
+                    <input type="checkbox" name="${CONSTANTS.FLAGS.ATTACH_REGION_TO_TEMPLATE}" ${attachRegionToTemplate ? 'checked' : ''}>
                     ${game.i18n.localize("REGION-ATTACHER.AttachRegionToTemplate")}
                 </label>
             </div>
@@ -23,5 +24,5 @@ function patchItemSheet(app, html, item) {
     if (!targetTypeElem) return;
     if (!Object.keys(CONFIG.DND5E.areaTargetTypes).includes(targetTypeElem.value)) return;
     let targetElem = targetTypeElem.parentNode.parentNode;
-    $(getAttachRegionHtml()).insertAfter(targetElem)
+    $(getAttachRegionHtml(item)).insertAfter(targetElem)
 }
